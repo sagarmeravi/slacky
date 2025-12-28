c76675f703cfbbb90f071c8ad9a954a21e91534b
sagarmeravi <sagarmeravi563@gmail.com>
2025-12-28 12:00:40 +0530

    auth implemented

---
+++ backend/src/config/inngest.js
@@ -0,0 +1,36 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "slacky" });
+
+const syncUser = inngest.createFunction(
+  { id: "sync-user" },
+  { event: "clerk/user.creation" },
+  async ({ event }) => {
+    await connectDB();
+
+    const { id, email_addresses, first_name, last_name, image_url } =
+      event.data; // Contains the Clerk user data
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}`,
+      imageUrl: image_url,
+    };
+    await User.create(newUser);
+    //todo: etc.
+  }
+);
+const deleteUserFromDB = inngest.createFunction(
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+    await deleteStreamUser(id.toString());
+    //todo etc
+  }
+);
+// Create an empty array where we'll export future Inngest functions
+export const functions = [syncUser, deleteUserFromDB];
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,35 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import { ENV } from "./config/env.js";
+import { clerkMiddleware } from "@clerk/express";
 import { connectDB } from "./config/db.js";
+import { functions, inngest } from "./config/inngest.js";
+import { serve } from "inngest/express";
 const app = express();
+
+app.use(express.json());
+app.use(clerkMiddleware()); //req.auth will be
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.send("Hello world! 123");
 });
 
-app.listen(ENV.PORT, () => {
-  console.log("server is started at ", ENV.PORT);
-  connectDB();
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log("server is started at ", ENV.PORT);
+        connectDB();
+      });
+    }
+  } catch (error) {
+    console.log("Error in starting server ", error);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+export default app;
