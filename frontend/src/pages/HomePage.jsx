814b0c345e1b839fb6acd3e820d1b6ea798c65dc
sagarmeravi <sagarmeravi563@gmail.com>
2025-12-30 10:59:21 +0530

    auth page implemented

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { UserButton } from "@clerk/clerk-react";
+function HomePage() {
+  return (
+    <>
+      <UserButton></UserButton>
+    </>
+  );
+}
+
+export default HomePage;
