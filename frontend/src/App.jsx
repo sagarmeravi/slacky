bf6f9307d645dae6100fed919edd593d1513a1fd
sagarmeravi <sagarmeravi563@gmail.com>
2026-01-01 12:18:50 +0530

    auth prover added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,22 +7,25 @@ import {
 import { Routes, Route, Navigate } from "react-router";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
+import * as Sentry from "@sentry/react";
+
+const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 
 const App = () => {
   return (
     <>
       <SignedIn>
-        <Routes>
+        <SentryRoutes>
           <Route path="/" element={<HomePage />} />
           <Route path="/auth" element={<Navigate to={"/"} replace />} />
-        </Routes>
+        </SentryRoutes>
       </SignedIn>
 
       <SignedOut>
-        <Routes>
+        <SentryRoutes>
           <Route path="/auth" element={<AuthPage />} />
           <Route path="*" element={<Navigate to={"/auth"} replace />} />
-        </Routes>
+        </SentryRoutes>
       </SignedOut>
     </>
   );
